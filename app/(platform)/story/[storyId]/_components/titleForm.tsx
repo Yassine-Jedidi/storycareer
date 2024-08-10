d70997378e5d396eff431eab942eee754dec9098
Yassine <yassinejedidi17@gmail.com>
2024-08-10 13:21:06 +0100

    update story

--- app/(platform)/story/[storyId]/_components/titleForm.tsx
+++ app/(platform)/story/[storyId]/_components/titleForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Story } from "@prisma/client";
 import React, { useState } from "react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -18,39 +19,43 @@ import {
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@clerk/nextjs";
-import { Loader } from "lucide-react";
+import { Loader, Pencil } from "lucide-react";
 import axios from "axios";
 import { toast } from "sonner";
+
+interface Props {
+  initialData: Story;
+}
 
 const formSchema = z.object({
   title: z.string().min(2).max(500),
 });
 
-const NewStoryPage = () => {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const { userId } = useAuth();
+const TitleForm = ({ initialData }: Props) => {
   const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      title: "",
+      title: initialData.title || "",
     },
   });
 
-  // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       setIsSubmitting(true);
-      toast("Story is being created ...");
-      const response = await axios.post("/api/story", {
+      toast("Story is being updated ...");
+      const response = await axios.patch(`/api/story/${initialData.id}`, {
         ...values,
-        userId,
       });
 
       if (response.status === 201) {
-        toast("Story is created", { className: "bg-emerald-500 text-white" });
+        toast("Story is updated", { className: "bg-emerald-500 text-white" });
       }
-      router.push(`/story/${response.data.id}`);
+      toggleEdit();
       router.refresh();
     } catch (error) {
       toast("Something went wrong :(", { className: "bg-rose-500 text-white" });
@@ -58,15 +63,25 @@ const NewStoryPage = () => {
       setIsSubmitting(false);
     }
   }
+
   return (
-    <div className="flex justify-center items-center h-full w-full">
-      <div className="flex flex-col space-y-10">
-        <div>
-          <h1 className="text-2xl font-semibold">Give your story a title</h1>
-          <p className="text-sm text-muted-foreground">
-            Name your story, don't worry you can change it later.
-          </p>
-        </div>
+    <div className="border border-secondary rounded-md p-4 bg-secondary w-80">
+      <div className="flex items-center justify-between font-medium text-primary">
+        Title
+        <Button variant={"ghost"} type="button" onClick={toggleEdit}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit Title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing ? (
+        <p className="text-sm mt-2 text-primary">{initialData.title}</p>
+      ) : (
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <FormField
@@ -83,13 +98,13 @@ const NewStoryPage = () => {
             />
             <Button disabled={isSubmitting} type="submit">
               {isSubmitting && <Loader className="mr-2 h-2 w-2 animate-spin" />}
-              Create
+              Update
             </Button>
           </form>
         </Form>
-      </div>
+      )}
     </div>
   );
 };
 
-export default NewStoryPage;
+export default TitleForm;
